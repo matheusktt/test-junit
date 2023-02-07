33ec7b262e25805656e6d9d2a76c8541f7c41b73
matheusktt <matheus_0119@outlook.com>
2023-02-07 15:49:01 -0300

    test deposit

--- src/test/java/entities/AccountTests.java
+++ src/test/java/entities/AccountTests.java
@@ -1,5 +1,6 @@
 package entities;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AccountTests {
@@ -7,5 +8,12 @@ public class AccountTests {
     @Test
     public void depositShouldIncreseBalanceWhenPositiveAmount() {
 
+        double amount = 200.0;
+        double expectedValue = 196.0;
+
+        Account acc = new Account(1L, 0.0);
+        acc.deposit(amount);
+
+        Assertions.assertEquals(expectedValue, acc.getBalance());
     }
 }
