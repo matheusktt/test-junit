d94b616a7e2ca1294b99fd3bcf550b679811647b
matheusktt <matheus_0119@outlook.com>
2023-02-08 19:53:15 -0300

    account tests

--- src/test/java/entities/AccountTests.java
+++ src/test/java/entities/AccountTests.java
@@ -16,4 +16,46 @@ public class AccountTests {
 
         Assertions.assertEquals(expectedValue, acc.getBalance());
     }
+
+    @Test
+    public void depositShouldDoNothingWhenNegativeAmount() {
+
+        double amount = -500.0;
+        double expectedValue = 200;
+
+        Account acc =  new Account(1L, expectedValue);
+        acc.deposit(amount);
+
+        Assertions.assertEquals(expectedValue, acc.getBalance());
+    }
+
+    @Test
+    public void fullWithdrawShouldClearBalance() {
+
+        double expectedValue =  0.0;
+        double initialBalance = 800.0;
+
+        Account acc = new Account(1L, initialBalance);
+        acc.fullWithdraw();
+
+        Assertions.assertTrue(expectedValue == acc.getBalance());
+    }
+
+    @Test
+    public void withdrawShouldDecreaseBalanceWhenSufficientBalance() {
+
+        Account acc = new Account(1L, 800.0);
+        acc.withdraw(500);
+
+        Assertions.assertEquals(300, acc.getBalance());
+    }
+
+    @Test
+    public void withdrawShouldThrowExceptionWhenInsufficientBalance() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Account acc = new Account(1L, 800.0);
+            acc.withdraw(900);
+        });
+    }
 }
